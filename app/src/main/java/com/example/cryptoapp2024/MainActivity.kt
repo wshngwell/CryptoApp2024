f87bda21deb6146b06a2aaadd2d2e3af72b85350
= <wishingwelltut@gmail.com>
2024-07-30 20:24:32 +0300

    add network and create db without dao

--- app/src/main/java/com/example/cryptoapp2024/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp2024/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.cryptoapp2024
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cryptoapp2024.data.network.ApiFactory
+import com.example.cryptoapp2024.data.network.DtoClasses.GetFullDataOfCoinsDto.CoinFullInfoDto
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 }
